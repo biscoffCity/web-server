32af7cb11e82d8c63335ffb735d6b4f0e3bd40fb
Adnan Mutee <am6377@nyu.edu>
2016-08-09 01:26:36 -0400

    Update language

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication,function (req, res) {
-	res.send('About us!');
+	res.send('About us!!');
 });
 
 app.use(express.static(__dirname+'/public')); //express.static is used for static files like CSS, images ,JavaSscript
